70db2faf31e5fcc4a9862f35aafee5e062263210
Arpan Kumar <karpan097@gmail.com>
2021-07-19 17:15:07 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {
-    path :'',
-    redirectTo :'login',
-    pathMatch : 'full' 
-  },
+  
   {
     path : 'login',
     component : LoginComponent
